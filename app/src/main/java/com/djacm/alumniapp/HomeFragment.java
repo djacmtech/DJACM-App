bdb968193c73513ac61460bf9003c94148fe604b
Aditi Kamath <adikamath3@gmail.com>
2021-02-21 13:28:51 +0530

    Codebash event changes added

--- app/src/main/java/com/djacm/alumniapp/HomeFragment.java
+++ app/src/main/java/com/djacm/alumniapp/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.djacm.alumniapp;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -8,14 +9,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,9 +41,13 @@ public class HomeFragment extends Fragment {
     SharedPreferences.Editor editor;
     FirebaseDatabase db;
     TextView aboutus_firstpara,aboutus_secondpara,vision_msg,mission_para1,mission_para2;
+    TextView roundno1,roundno2,status_upcoming,status_ongoing,round1_date,round2_date;
+    View upcoming_circle_view,ongoing_circle_view;
+    ImageView codebash_img;
     CardView event_cardview;
     Uri event_link;
     LinearLayout upcomingEventCardLayout;
+    RelativeLayout codebash_layout;
 
     DatabaseReference reference;
     public HomeFragment()
@@ -64,6 +72,15 @@ public class HomeFragment extends Fragment {
         eventregister = v.findViewById(R.id.event_register_button);
         event_cardview = v.findViewById(R.id.event_cardview);
         upcomingEventCardLayout = v.findViewById(R.id.upcoming_events);
+        roundno1 = v.findViewById(R.id.roundno1);
+        roundno2 = v.findViewById(R.id.roundno2);
+        round1_date =v.findViewById(R.id.round_date1);
+        round2_date = v.findViewById(R.id.round_date2);
+        upcoming_circle_view = v.findViewById(R.id.upcoming_circle_view);
+        ongoing_circle_view = v.findViewById(R.id.ongoing_circle);
+        status_upcoming = v.findViewById(R.id.round_status_upcoming);
+        status_ongoing = v.findViewById(R.id.round_status_ongoing);
+        codebash_img = v.findViewById(R.id.codebash_img);
 
         eventregister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,12 +98,14 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        codebash_layout = v.findViewById(R.id.codebash_layout);
         getData();
         event_name = v.findViewById(R.id.eventname);
         upcoming_event_pic = v.findViewById(R.id.upcoming_event_pic);
         event_date = v.findViewById(R.id.eventdate);
         event_desc = v.findViewById(R.id.eventdesc);
         getEventData();
+        getCodebashData();
         return v;
     }
     private void getData(){
@@ -135,6 +154,53 @@ public class HomeFragment extends Fragment {
                 }
                 else
                     upcomingEventCardLayout.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void getCodebashData(){
+        reference = db.getReference("Codebash");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int iscodebashActive =  Integer.parseInt(dataSnapshot.child("isActive").getValue().toString());
+                int isOngoing = Integer.parseInt(dataSnapshot.child("isOngoing").getValue().toString());
+                // ongoing ==1 text color and view color green
+                // ongoing ==0 text and view color red
+                if(iscodebashActive==1)
+                {
+                    codebash_layout.setVisibility(View.VISIBLE);
+                    String date1 = dataSnapshot.child("roundno1date").getValue().toString();
+                    String status1 = dataSnapshot.child("roundno1status").getValue().toString();
+                    String roundnum1 = dataSnapshot.child("roundno1").getValue().toString();
+                    String date2 = dataSnapshot.child("roundno2-date").getValue().toString();
+                    String status2 = dataSnapshot.child("roundno2status").getValue().toString();
+                    String roundnum2 = dataSnapshot.child("roundno2").getValue().toString();
+                    String imageUri = dataSnapshot.child("codebashimg").getValue().toString();
+                    if(isOngoing==1){
+                        status_ongoing.setText(status1);
+                        status_ongoing.setTextColor(getResources().getColor(R.color.ongoingcolor));
+                        ongoing_circle_view.setBackground(ContextCompat.getDrawable(getContext(),R.drawable.ongoing_circle));
+                    }else{
+                        status_ongoing.setText(status1);
+                        status_ongoing.setTextColor(getResources().getColor(R.color.upcomingcolor));
+                        ongoing_circle_view.setBackground(ContextCompat.getDrawable(getContext(),R.drawable.upcoming_circle));
+                    }
+                    roundno1.setText(roundnum1);
+                    roundno2.setText(roundnum2);
+
+                    status_upcoming.setText(status2);
+                    round1_date.setText(date1);
+                    round2_date.setText(date2);
+                    if (getActivity() != null)
+                        Glide.with(getActivity()).load(imageUri).into(codebash_img);
+                }else{
+                    codebash_layout.setVisibility(View.GONE);
+                }
             }
 
             @Override
